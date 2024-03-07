fcc43b9b7271f6ad00836aba0038229d38dcc315
XZT008 <zx581@nyu.edu>
2024-03-07 12:49:16 +0800

    Initial Commit

--- baselines/run_script.py
+++ baselines/run_script.py
@@ -6,7 +6,7 @@ from benchmark.naslib_benchmark import NasBench201
 from benchmark.svm_benchmark import SVMBenchmark
 from benchmark.mopta8 import MoptaSoftConstraints
 from benchmark.real_dataset import RealDataset
-from BO_loop import BO_loop_GP
+from BO_loop import BO_loop_GP, BO_loop_SaasBO_MAP, BO_loop_GP_pyro, BO_loop_GP_EI
 from benchmark.DNA import DNA_Lasso
 import click
 
@@ -23,8 +23,9 @@ class Config:
 def all_configs():
     config_list = []
     for seed in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
-        for model_name in ['GP_ARD', 'GP']:
-            for func_name in ['mopta08', 'rover', 'nas201', 'dna', 'SVM', 'Ackley', 'Ackley150', 'StybTang_V1', 'Rosenbrock_V1', 'Rosenbrock100_V1', 'Hartmann6']:
+        for model_name in ['GP_ARD', 'GP', 'GP_ARD_PYRO', 'GP_PYRO', 'SaasBO_MAP']:
+            for func_name in ['mopta08', 'rover', 'nas201', 'dna', 'SVM', 'Ackley', 'Ackley150', 'StybTang_V1',
+                              'Rosenbrock_V1', 'Rosenbrock100_V1', 'Hartmann6']:
                 for beta in [1.5]:
                     for if_softplus in [True]:
                         config = Config(func_name, model_name, seed, beta, if_softplus)
@@ -41,12 +42,13 @@ def get_config(index):
 @click.command()
 @click.option("--index", type=int, required=True, help="Which grid index to run.")
 def main(index):
+    cwd = os.getcwd()
     config = get_config(index)
     model_name = config.model_name
     SEED = config.seed
     func_name = config.func_name
     beta = config.beta
-    beta_out = int(10*beta)
+    beta_out = int(10 * beta)
     if_softplus = config.if_softplus
 
     torch.manual_seed(SEED)
@@ -86,7 +88,7 @@ def main(index):
         D = 150
         func = FuncAckley(D, maximize=True)
         dst = BayesOptDataset(func, 20, 'lhs', SEED)
-        
+
     elif func_name == 'StybTang':
         num_step = 400
         D = 200
@@ -98,7 +100,7 @@ def main(index):
         D = 200
         func = FuncStybTang_V1(D, maximize=True)
         dst = BayesOptDataset(func, 20, 'lhs', SEED)
-        
+
     elif func_name == 'Rosenbrock100':
         num_step = 400
         D = 300
@@ -135,6 +137,12 @@ def main(index):
         best_val, time_list = BO_loop_GP(dst, SEED, num_step=num_step, beta=beta, if_ard=False, if_softplus=if_softplus)
     elif model_name == 'GP_ARD':
         best_val, time_list = BO_loop_GP(dst, SEED, num_step=num_step, beta=beta, if_ard=True, if_softplus=if_softplus)
+    elif model_name == 'GP_ARD_PYRO':
+        best_val, time_list = BO_loop_GP_pyro(dst, SEED, num_step=num_step, beta=1.5, if_ard=True, if_softplus=True)
+    elif model_name == 'GP_PYRO':
+        best_val, time_list = BO_loop_GP_pyro(dst, SEED, num_step=num_step, beta=1.5, if_ard=False, if_softplus=True)
+    elif model_name == 'SaasBO_MAP':
+        best_val, time_list = BO_loop_SaasBO_MAP(dst, num_step=num_step, acqf="EI")
     else:
         raise NotImplementedError
 
@@ -143,7 +151,23 @@ def main(index):
         "X": dst.X,
         "Y": dst.y
     }
-    with open(f'/scratch/zx581/bo_baseline/baselines/gp_output/{model_name}_{func_name}_{SEED}_{beta_out}_{if_softplus}_10.pickle', 'wb') as handle:
+
+    if model_name in ['GP', 'GP_ARD']:
+        output_dir = os.path.join(cwd, "gp_output")
+        os.makedirs(output_dir, exist_ok=True)
+        output_file = os.path.join(output_dir, f"{model_name}_{func_name}_{SEED}_{beta_out}_{if_softplus}_10.pickle'")
+    elif model_name in ['GP_ARD_PYRO', 'GP_PYRO']:
+        output_dir = os.path.join(cwd, "gp_pyro_output")
+        os.makedirs(output_dir, exist_ok=True)
+        output_file = os.path.join(output_dir, f"{model_name}_{func_name}_{SEED}.pickle'")
+    elif model_name in ['SaasBO_MAP']:
+        output_dir = os.path.join(cwd, "saasbo_output")
+        os.makedirs(output_dir, exist_ok=True)
+        output_file = os.path.join(output_dir, f"{model_name}_{func_name}_{SEED}.pickle'")
+    else:
+        raise NotImplementedError
+
+    with open(output_file, 'wb') as handle:
         pickle.dump(BO_result, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
